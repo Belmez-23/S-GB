34d104ca5d5f883c39b5263361f1abd4eff68149
Username <afoniabrando@mail.ru>
2021-07-22 14:32:28 +0300

    шаг 17.6 загружены фикстуры (заполнена тестовая БД)

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Admin;
+use App\Entity\Comment;
+use App\Entity\Conference;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\EncoderFactoryInterface;
+
+class AppFixtures extends Fixture
+{
+    private $encoderFactory;
+
+    public function __construct(EncoderFactoryInterface $encoderFactory)
+    {
+        $this->encoderFactory = $encoderFactory;
+    }
+    public function load(ObjectManager $manager)
+    {
+        // $product = new Product();
+        // $manager->persist($product);
+        $canterlot = new Conference();
+        $canterlot->setCity('Canterlot');
+        $canterlot->setYear('2019');
+        $canterlot->setIsInternational(true);
+        $manager->persist($canterlot);
+    
+        $laspegasus = new Conference();
+        $laspegasus->setCity('Las-Pegasus');
+        $laspegasus->setYear('2020');
+        $laspegasus->setIsInternational(false);
+        $manager->persist($laspegasus);
+    
+        $comment1 = new Comment();
+        $comment1->setConference($canterlot);
+        $comment1->setAuthor('Sunburst');
+        $comment1->setEmail('sun@example.com');
+        $comment1->setText('This was a great conference.');
+        $manager->persist($comment1);
+
+        $admin = new Admin();
+        $admin->setRoles(['ROLE_ADMIN']);
+        $admin->setUsername('admin');
+        $admin->setPassword($this->encoderFactory->getEncoder(Admin::class)->encodePassword('admin', null));
+        $manager->persist($admin);
+
+        $manager->flush();
+    }
+}
